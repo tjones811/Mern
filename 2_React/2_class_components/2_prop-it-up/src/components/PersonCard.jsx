1e2c215c0bb3f8be0d53a5236f75fe3eb67758a1
Trevon Jones <roboleaf811@gmail.com>
2022-05-09 10:36:46 -0500

    commit

--- 2_React/2_class_components/2_prop-it-up/src/components/PersonCard.jsx
+++ 2_React/2_class_components/2_prop-it-up/src/components/PersonCard.jsx
@@ -1,12 +1,21 @@
 import React , { Component } from 'react';
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+
     render() {
 
         let {lastName, firstName, age, hairColor,children} = this.props
 
-        function increaseAge(){
-
+        const increaseAge = ()=>{
+            this.setState({ age: this.state.age + 1})
         }
 
 
@@ -15,7 +24,7 @@ class PersonCard extends Component {
             <>
                 <div>
                     <h1>Name : {lastName}, {firstName}</h1>
-                    <h2>Age: {age} </h2>
+                    <h2>Age: {this.state.age} </h2>
                     <h2>Hair Color:  {hairColor} </h2>
                     {children}
                     <button onClick={increaseAge}>Birthday Button for {firstName} {lastName} </button>
