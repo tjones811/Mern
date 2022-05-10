43d9463116b2de2a4bf13eeb47e6422bad6034b2
Trevon Jones <roboleaf811@gmail.com>
2022-05-10 09:10:16 -0500

    commit

--- 2_React/3_functional_components/1-big-inversion/src/components/PersonCard.jsx
+++ 2_React/3_functional_components/1-big-inversion/src/components/PersonCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props)=>{
 
-    const {firstName,lastName,age,hairColor,children} = props;
-
+    const {firstName,lastName,hairColor,children} = props;
+    
+    //Use state when you want a variable that can change its value and the change in 
+    //its value needs to be reflected in the screen/window
+    let [age,setAge] = useState(props.age)
+    
+    const increaseAge = () => {
+        setAge(age + 1);
+    }
     return (
         <>
             <div>
@@ -11,6 +18,7 @@ const PersonCard = (props)=>{
                 <h2>Age: {age}</h2>
                 <h2>Hair Color: {hairColor} </h2>
                 {children}
+                <button onClick = {increaseAge}>Increase age for {firstName} {lastName}</button>
             </div>
             <hr />
         
